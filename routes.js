13a0ebcb1860a039f5ceedda4fdc703fba37fa22
Miguel Lima <miguelsousalima@hotmail.com>
2020-03-05 11:00:32 -0300

    Update of edit teacher.js

--- routes.js
+++ routes.js
@@ -24,5 +24,8 @@ routes.get('/teachers/:id', teachers.show);
 
 routes.get('/teachers/:id/edit', teachers.edit);
 
+routes.put('/teachers', teachers.put);
+
+routes.delete('/teachers', teachers.delete);
 
 module.exports = routes;
